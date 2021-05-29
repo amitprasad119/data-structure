ca177cb5100ead6e45e71cc8662191eda6970739
Amit Prasad <amiprasa@WKMZT8B46FA4.global.publicisgroupe.net>
2021-05-29 12:33:08 +0530

    zero one segregation one more approach

--- src/main/java/com/amit/problemsolving/ZeroOneSegregation.java
+++ src/main/java/com/amit/problemsolving/ZeroOneSegregation.java
@@ -1,12 +1,16 @@
 package com.amit.problemsolving;
 
-public class ZeroOneSegration {
+public class ZeroOneSegregation {
 
     public static void main(String[] args) {
         int[] a = {0,1,1,2,1,0,1,0,1,2};
         int[] b = {0,1,0,1,0,1,1,0,1};
         segregateZeroOneTwo(a);
         segregateZeroOne(b);
+        System.out.println("segregation using mid pointer");
+
+        for(int e: segregateZeroOneTwoUsingMid(a))
+            System.out.print(e + ",");
     }
 
  public static void segregateZeroOne(int[] a) {
@@ -28,6 +32,7 @@ public class ZeroOneSegration {
      System.out.println();
  }
 
+ // approach one
  public static void segregateZeroOneTwo(int[] a) {
      int oneCount = 0;
      int zeroCount = 0;
@@ -49,6 +54,34 @@ public class ZeroOneSegration {
     for(int ele:a)
         System.out.print(ele +",");
      System.out.println();
+ }
+
+//approach two
+ public static int[] segregateZeroOneTwoUsingMid(int[] a) {
+        int mid,start;
+        int end = a.length - 1;
+        mid = start = 0;
+
+        while(mid <= end) {
+            switch (a[mid]) {
+                case 0 : swap(a,start,mid);
+                         start++;
+                         mid++;
+                         break;
+                case 1 : mid++;
+                          break;
+                case 2 : swap(a,mid,end);
+                          end--;
+                          break;
+            }
+        }
+     return a;
+ }
+
+ public static void swap(int a[],int index1,int index2){
+      int temp = a[index1];
+      a[index1] = a[index2];
+      a[index2] = temp;
  }
 
 }

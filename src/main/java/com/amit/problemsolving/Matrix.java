2dd5ec1293c336b5a1c751de8de67b2544e92f1f
Amit Prasad <amiprasa@WKMZT8B46FA4.global.publicisgroupe.net>
2021-06-02 07:36:17 +0530

    matrix 90 percent image rotation

--- src/main/java/com/amit/problemsolving/Matrix.java
+++ src/main/java/com/amit/problemsolving/Matrix.java
@@ -2,18 +2,63 @@ package com.amit.problemsolving;
 
 public class Matrix {
     public static void main(String[] args) {
-        int mat[][] = {{1,2,3},{4,5,6},{7,8,9}};
 
-        for(int i =0; i< mat.length;i++) {
-            for(int j =0; j < mat.length;j++) {
-                System.out.print(mat[i][j] + " ");
+        /**
+                Original image
+                1 2 3
+                4 5 6
+                7 8 9
+
+                After transpose
+                1 4 7
+                2 5 8
+                3 6 9
+
+                After flip rotation
+                7 4 1
+                8 5 2
+                9 6 3
+
+           **/
+        int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        int row = matrix.length;
+        int col = matrix.length;
+        System.out.println("Original image");
+        for(int  i= 0;i< row; i++) {
+            for(int j=0;j<col;j++) {
+                System.out.print(matrix[i][j] +" ");
             }
             System.out.println();
         }
-        System.out.println("Transpose");
-        for(int i =0; i< mat.length;i++) {
-            for(int j =0; j < mat.length;j++) {
-                System.out.print(mat[j][i] + " ");
+
+        // transpose the matrix
+        for(int i =0;i<row;i++) {
+            for(int j = i+1; j<col;j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        System.out.println("After transpose");
+        for(int  i= 0;i< row; i++) {
+            for(int j=0;j<col;j++) {
+                System.out.print(matrix[i][j] +" ");
+            }
+            System.out.println();
+        }
+        // flip the matrix by swapping last column and first column and gradually
+        for(int i =0;i<row;i++) {
+            for(int j = 0; j< col/2;j++) {
+                int temp = matrix[i][col-1-j]; // start swapping last and first column and likewise
+                matrix[i][col-1-j] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+        System.out.println("After rotation");
+        for(int  i= 0;i< row; i++) {
+            for(int j=0;j<col;j++) {
+                System.out.print(matrix[i][j] +" ");
             }
             System.out.println();
         }
